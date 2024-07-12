768a09f115c93a5a3f3a3f84a1d1bdbe618f7870
quy.hoang <quy.hoang@kiaisoft.com.vn>
2024-07-12 08:52:56 +0700

    feat: seed data api

--- src/routes/seller.routes.ts
+++ src/routes/seller.routes.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { seller as createSeller } from '@users/controllers/seller/create';
 import { seller as updateSeller } from '@users/controllers/seller/update';
 import { id, random, username } from '@users/controllers/seller/get';
+import { seed } from '@users/controllers/seller/seed';
 
 const router: Router = express.Router();
 
@@ -12,6 +13,7 @@ const sellerRoutes = (): Router => {
   router.get('/id/:sellerId', id);
   router.get('/username/:username', username);
   router.get('/random/:size', random);
+  router.put('/put/:count', seed);
 
   return router;
 };
